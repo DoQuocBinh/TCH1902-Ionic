7f2d9edb4e0b6a84d839968db73c28db5a8ea998
Binh Do <binhdq@fe.edu.vn>
2021-08-11 12:02:14 +0700

    ok 4

--- mythirdapp/src/pages/Home.tsx
+++ mythirdapp/src/pages/Home.tsx
@@ -1,12 +1,22 @@
-import { IonButton, IonContent, IonHeader, IonInput, IonItem, IonLabel, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonDatetime, IonHeader, IonInput, IonItem, IonLabel, IonPage, IonSelect, IonSelectOption, IonTitle, IonToast, IonToolbar } from '@ionic/react';
 import { useState } from 'react';
 import './Home.css';
 
 const Home: React.FC = () => {
   const [name,setName]= useState('')
+  const [country, setCountry] = useState('')
+  const [languages, setLanguages] = useState<[]>()
+  const [showToast,setShowToast] = useState(false)
+  const [dob, setDob]= useState(new Date().toISOString())
   
   function clickHandler(){
-    alert(name);
+    setShowToast(true);
+    setTimeout(()=>{
+      setShowToast(false);
+    },2000)
+  }
+  function formatDate(isoString:string){
+    return new Date(isoString).toLocaleDateString("vi-VN");
   }
   return (
     <IonPage>
@@ -25,7 +35,35 @@ const Home: React.FC = () => {
           <IonLabel position="stacked">Name</IonLabel>
           <IonInput value={name} onIonChange={(event)=>setName(event.detail.value!)}></IonInput>
         </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">Country</IonLabel>
+          <IonSelect value={country} onIonChange={(event)=>setCountry(event.detail.value)}>
+            <IonSelectOption value="vn">VietNam</IonSelectOption>
+            <IonSelectOption value="indo">Indonesia</IonSelectOption>
+          </IonSelect>
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">Languages you can speak</IonLabel>
+          <IonSelect multiple={true} value={languages} onIonChange={(event)=>setLanguages(event.detail.value)}>
+            <IonSelectOption value="en">English</IonSelectOption>
+            <IonSelectOption value="spanish">Spanish</IonSelectOption>
+            <IonSelectOption value="chinese">Chinese</IonSelectOption>
+          </IonSelect>
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">Date of birth</IonLabel>
+          <IonDatetime value={dob} onIonChange={event=>setDob(event.detail.value!)}></IonDatetime>
+        </IonItem>
         <IonButton expand="block" onClick={clickHandler}>Ok</IonButton>
+        <IonToast message={"Hello " 
+                  + name + " " 
+                  + country + " "
+                + languages + " "
+                + formatDate(dob)} isOpen={showToast} position="middle"></IonToast>
+        
+        {languages &&
+          languages.map((item,i) => <IonItem color="success"><IonLabel>{i + "."+ item}</IonLabel></IonItem>)
+        }
       </IonContent>
     </IonPage>
   );
