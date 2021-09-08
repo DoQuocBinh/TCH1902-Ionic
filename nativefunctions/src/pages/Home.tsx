c2dea589d25d383ee0a2f32cc3111df0b4b32efb
Binh Do <binhdq@fe.edu.vn>
2021-09-08 11:25:00 +0700

    fileinput

--- nativefunctions/src/pages/Home.tsx
+++ nativefunctions/src/pages/Home.tsx
@@ -1,8 +1,16 @@
-import { IonButton, IonContent, IonHeader, IonItem, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonInput, IonItem, IonLabel, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { useState } from 'react';
 import ReactAudioPlayer from 'react-audio-player';
 
 const Home: React.FC = () => {
   var musicPlayer: ReactAudioPlayer | null
+  const [pictureURL,setPictureURL] = useState<string>('')
+
+  const handleSelectPicture = (event: React.ChangeEvent<HTMLInputElement>)=>{
+    if(event.target.files && event.target.files.length > 0){
+      alert('you selected: '+ event.target.files[0].name)
+    }
+  }
   return (
     <IonPage>
       <IonHeader>
@@ -12,8 +20,8 @@ const Home: React.FC = () => {
       </IonHeader>
       <IonContent fullscreen>
         <IonItem>
-          <IonButton onClick={() => musicPlayer?.audioEl.current?.play()}>Play</IonButton>
-          <IonButton onClick={() => musicPlayer?.audioEl.current?.pause()}>Pause</IonButton>
+          <IonButton onClick={() => musicPlayer?.audioEl.current?.play()}>Play music</IonButton>
+          <IonButton onClick={() => musicPlayer?.audioEl.current?.pause()}>Pause music</IonButton>
           <ReactAudioPlayer
             ref={(element) => { musicPlayer = element; }}
             src="./assets/music.mp3"
@@ -22,6 +30,17 @@ const Home: React.FC = () => {
         </IonItem>
         <IonItem>
           <IonButton onClick={()=>navigator.vibrate(2000)}>Vibrate</IonButton>
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">Picture name</IonLabel>
+          <IonInput></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">Select a picture</IonLabel>
+          <input type="file" accept="image/*" onChange={handleSelectPicture}></input>
+        </IonItem>
+        <IonItem>
+          <img src={pictureURL} width="120" height="100"/>
         </IonItem>
       </IonContent>
     </IonPage>
